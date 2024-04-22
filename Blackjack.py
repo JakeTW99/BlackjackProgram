e6863bb71837ebe744b847eb76e96154053bffa6
JakeTW99 <jaketw99@gmail.com>
2024-04-22 18:01:07 -0400

    Able to play game with one error
    Ace does not change value when total goes over 21.

--- Blackjack.py
+++ Blackjack.py
@@ -4,28 +4,41 @@ card_suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
 list_of_cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 deck = [(card, suit) for suit in card_suits for card in list_of_cards]
 
-#print(deck)
-
 def card_value(card):
-    if card == "Jack":
+    if card[0] in ["Jack", "Queen", "King"]:
         return 10
-    elif card == "Queen":
-        return 10
-    elif card == "King":
-        return 10
-    elif card == "Ace":
+    elif card[0] == "Ace":
         return 11
     else:
-        return int((card))
-
-#print (card_value("Jack"))
+        return int(card[0])
 
 random.shuffle(deck)
 
 player_cards = [deck.pop(), deck.pop()]
+player_cards_total = sum(card_value(card) for card in player_cards)
 
-#print("You have a " + player_cards[0] + ".")
-#print("You have a " + player_cards[1] + ".")
+print(player_cards)
 
-print("Your first card is: (player_cards[0])")
-
+while True:
+    if player_cards_total < 21:
+        choice = input("Your total is " + str(player_cards_total) + ". Would you like to hit or stand?: ")
+        if choice == "hit":
+            player_cards.append(deck.pop())
+            print(player_cards[-1])
+            player_cards_total += card_value(player_cards[-1])
+            print("Your total is now " + str(player_cards_total) + ".")
+        elif choice == "stand":
+            print("Your final total is " + str(player_cards_total) + ".")
+            break
+        else:
+            print("Invalid input. Please type \"hit\" or \"stand\". ")
+    elif player_cards_total == 21:
+        print("Congratulations! You've won!")
+        break
+    elif player_cards_total > 21:
+        if ("Ace", 11) in player_cards:
+            player_cards_total -= 10
+            continue
+        else:
+            print("You have bust! Better luck next time!")
+            break
